3103d04937dd15adbe6f769b58ed45883f718bbe
josagumat <uiooui@gdffg>
2023-02-02 19:20:00 +0100

    sdffsd

--- src/org/benigaslo/Main.java
+++ src/org/benigaslo/Main.java
@@ -4,6 +4,8 @@ package org.benigaslo;
 import org.benigaslo.controller.Controlador;
 
 public class Main {
+
+    // variable
     public static void main(String[] args) {
         new Controlador().aCorrer();
 
